477f83375c840e5c4e78416a344302ab499483f5
Ahmed <ahmedshatat025@gmail.com>
2025-06-20 19:33:01 +0300

    Add project files.

---
+++ Coord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    public class Coord
+    {
+        private int x;
+        private int y;
+
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            Coord other = (Coord)obj;
+            return this.x == other.x && this.y == other.y;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using SnakeGame;
+
+Coord PlayingArea = new Coord(60, 25);
+Coord SnakePosition = new Coord(10, 1);
+Random random = new Random();
+Coord applePos = new Coord(random.Next(1, PlayingArea.X - 1), random.Next(1, PlayingArea.Y - 1));
+
+for (int y=0; y < PlayingArea.Y; y++) {
+    for(int x = 0; x < PlayingArea.X; x++) {
+        // Check if the current coordinate is the snake's position, apple's position, or a wall
+        Coord currentCoord = new Coord(x, y);
+
+        if (SnakePosition.Equals(currentCoord))
+            Console.Write(">");
+        else if (applePos.Equals(currentCoord))
+            Console.Write("a");
+        else if(x == 0 || y == 0 || y == PlayingArea.Y - 1 || x == PlayingArea.X - 1)
+            Console.Write("#");
+        else
+            Console.Write(" ");
+    }
+    Console.WriteLine();
+}
